91bbd80363bbe14178dc7455cff0a3beab9d3435
ZYARCX <soumya.lodh206@gmail.com>
2025-11-13 17:22:13 +0530

    Remove unused imports and state management from App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import Header from './components/Header.jsx';
-import MenuItem from './components/MenuItem.jsx';
-import { initialMenuItems } from './data/MenuItems.js';
-
 const App = () => {
-  // const [cart, setCart] = useState([]);
 
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
 
   return (
     <div>
